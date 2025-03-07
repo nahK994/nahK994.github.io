47869ff2b0429e504adae7e64338296e2914db7f
nahK994 <nkskl6@gmail.com>
2025-03-07 23:27:21 +0600

    add small screen design on skill

--- app/components/skills/item/skill-item.tsx
+++ app/components/skills/item/skill-item.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function SkillItem(item: { name: string, logo: string }) {
     return (
-        <div key={item.name} className="flex flex-col items-center bg-white rounded-2xl shadow-md p-2 w-28">
+        <div key={item.name} className="flex flex-col items-center bg-white rounded-2xl shadow-md p-2 w-28 hover:scale-105 hover:shadow-lg cursor-pointer">
             <Image src={item.logo} alt={item.name} width={50} height={50} className="w-10 h-10 mb-4" />
             <h3 className="text-lg font-medium text-green-600">{item.name}</h3>
         </div>
