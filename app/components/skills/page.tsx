e444810489c774a898745697d09864373b31eca6
nahK994 <nkskl6@gmail.com>
2025-03-07 23:27:21 +0600

    add small screen design on skill

--- app/components/skills/page.tsx
+++ app/components/skills/page.tsx
@@ -4,23 +4,23 @@ import SkillItem from "./item/skill-item";
 export default function Skills() {
     return (
         <section id={navItems[2].id} className="min-h-screen flex items-center justify-center bg-gradient-to-b from-green-50 to-yellow-50">
-            <div className="flex flex-col gap-8">
-                <div className="flex flex-row justify-center items-center gap-8">
+            <div className="flex flex-col gap-8 px-12">
+                <div className="flex flex-row flex-wrap justify-center items-center gap-8">
                     {Object.values(skill.language).map((s) => (
                         <SkillItem key={s.name} logo={s.logo} name={s.name} />
                     ))}
                 </div>
-                <div className="flex flex-row justify-center items-center gap-8">
+                <div className="flex flex-row flex-wrap justify-center items-center gap-8">
                     {Object.values(skill.backend).map((s) => (
                         <SkillItem key={s.name} logo={s.logo} name={s.name} />
                     ))}
                 </div>
-                <div className="flex flex-row justify-center items-center gap-8">
+                <div className="flex flex-row flex-wrap justify-center items-center gap-8">
                     {Object.values(skill.frontend).map((s) => (
                         <SkillItem key={s.name} logo={s.logo} name={s.name} />
                     ))}
                 </div>
-                <div className="flex flex-row justify-center items-center gap-8">
+                <div className="flex flex-row flex-wrap justify-center items-center gap-8">
                     {Object.values(skill.tools).map((s) => (
                         <SkillItem key={s.name} logo={s.logo} name={s.name} />
                     ))}
