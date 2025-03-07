6e27cd6ac4394cecac9a6e0c65441118fbbe6618
nahK994 <nkskl6@gmail.com>
2025-03-07 12:10:35 +0600

    refactor portfolio

---
+++ app/utils/constants.tsx
@@ -0,0 +1,34 @@
+export const navItems = [
+    { id: "", label: "Home", icon: "🏠" },
+    { id: "about", label: "About", icon: "🙋‍♂️" },
+    { id: "skills", label: "Skills", icon: "🧩" },
+    { id: "projects", label: "Projects", icon: "💡" },
+    { id: "blogs", label: "Bolgs", icon: "✍️" },
+];
+
+export const hero = {
+    title: "Hi, I’m Shomi Khan",
+    subTitle: "Software Engineer | Curious Soul 🤔💭 | Python Charmer 🐍 | Golang Alchemist 🦫"
+}
+
+export const aboutMe = {
+    myself: "I'm Shomi Khan, backend focused full-stack software engineer 🦫🖥️🐍 from Bangladesh 🇧🇩.",
+    passion: "🌥️ Enthusiastic about building cloud-native solutions, 🧑‍💻 writing clean code, ⚙️ diving into low-level systems, 🤖 automating everything possible",
+    hobby: "Apart from coding, I’m an anime fan ⛩️, former cyclist 🚴, and hobbyist photographer 📸",
+    welcome: "I love to chat 🗨️ rather than talk. If you ping me with a 'hi', I'll be happy to chat with you more! 😊"
+}
+
+export const profile = {
+    github: {
+        url: "https://github.com/nahK994",
+        logoUrl: "https://cdn-icons-png.flaticon.com/512/25/25231.png"
+    },
+    facebook: {
+        url: "https://www.facebook.com/shomi.khan1",
+        logoUrl: "https://cdn-icons-png.flaticon.com/512/174/174848.png"
+    },
+    linkedin: {
+        url: "https://linkedin.com/in/shomikhan",
+        logoUrl: "https://cdn-icons-png.flaticon.com/512/174/174857.png"
+    }
+}
