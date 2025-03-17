78323aa1a074d237922bc45de9d0166df4d61ee7
nahK994 <nkskl6@gmail.com>
2025-03-17 14:04:17 +0600

    update experience component

--- app/components/experiences/page.tsx
+++ app/components/experiences/page.tsx
@@ -4,21 +4,21 @@ import { experiences, navItems } from "../../utils/constants";
 export default function Experiences() {
     return (
         <section id={navItems[3].id} className="flex justify-center items-center min-h-screen font-mono px-3 bg-gradient-to-b to-green-50 from-yellow-50 text-gray-900">
-            <div className="max-w-4xl mx-auto px-4 py-20 w-fit">
-                <div className="relative border-l-4 border-green-600 w-fit sm:w-150">
-                    {experiences.map((exp, index) => (
-                        <div key={index} className={`mb-8 ml-6 ${index === experiences.length - 1 ? 'border-l-0' : ''}`}>
-                            <div className="absolute w-4 h-4 bg-green-600 rounded-full -left-2.5"></div>
-                            <h3 className="text-2xl font-semibold text-green-800">{exp.role}</h3>
-                            <p className="text-lg text-green-700">{exp.company} | {exp.period}</p>
+            <div className="bg-white border-2 border-dashed border-red-400 rounded-2xl shadow-lg py-8 px-4 max-w-fit">
+                {experiences.map((exp, index) => (
+                    <div key={index} className={`w-[80%] sm:w-150 -left-2 relative ml-6 ${index === experiences.length - 1 ? 'border-l-0 mb-0' : 'pb-8 border-yellow-100 border-l-2'}`}>
+                        <div className="absolute w-3 h-3 bg-green-300 rounded-full -left-1.5"></div>
+                        <div className="ml-6">
+                            <h3 className="text-xl font-semibold text-green-800">{exp.role}</h3>
+                            <p className="text-lg text-green-700"><span className="text-blue-700">{exp.company}</span> | {exp.period}</p>
                             <ul className="list-disc list-inside mt-2 text-green-900">
                                 {exp.responsibilities.map((resp, idx) => (
                                     <li key={idx}>{resp}</li>
                                 ))}
                             </ul>
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
         </section>
     );
