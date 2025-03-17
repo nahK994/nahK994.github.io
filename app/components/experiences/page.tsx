c22fb41e4b4ee29f9d26fa6f0537da70ae637dd8
nahK994 <nkskl6@gmail.com>
2025-03-17 13:01:38 +0600

    update experience component style

--- app/components/experiences/page.tsx
+++ app/components/experiences/page.tsx
@@ -4,8 +4,8 @@ import { experiences, navItems } from "../../utils/constants";
 export default function Experiences() {
     return (
         <section id={navItems[3].id} className="flex justify-center items-center min-h-screen font-mono px-3 bg-gradient-to-b to-green-50 from-yellow-50 text-gray-900">
-            <div className="max-w-4xl mx-auto px-4 py-20 w-full">
-                <div className="relative border-l-4 border-green-600 w-full sm:w-150">
+            <div className="max-w-4xl mx-auto px-4 py-20 w-fit">
+                <div className="relative border-l-4 border-green-600 w-fit sm:w-150">
                     {experiences.map((exp, index) => (
                         <div key={index} className={`mb-8 ml-6 ${index === experiences.length - 1 ? 'border-l-0' : ''}`}>
                             <div className="absolute w-4 h-4 bg-green-600 rounded-full -left-2.5"></div>
