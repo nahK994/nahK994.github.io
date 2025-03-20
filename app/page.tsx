cf4caaca66060884cc0ecbc3f925bde6a56d50a3
shomi-khan <shomikhan043@gmail.com>
2025-03-20 14:16:09 +0600

    refactor all components

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,17 @@ import Experiences from "./components/experiences/page";
 import Hero from "./components/hero/page";
 import Sidebar from "./components/sidebar/page";
 import Skills from "./components/skills/page";
+import { aboutMe, experiences, hero, navItems, profile, skill } from "./utils/constants";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
         <Sidebar />
-        <Hero />
-        <About />
-        <Skills />
-        <Experiences/>
-        <ContactMe />
+        <Hero sectionId={navItems[0].id} githubUrl={profile.github.url} hero={hero} />
+        <About sectionId={navItems[1].id} aboutMe={aboutMe} />
+        <Skills sectionId={navItems[2].id} skill={skill} />
+        <Experiences sectionId={navItems[3].id} experiences={experiences} />
+        <ContactMe sectionId={navItems[4].id} profile={profile} welcome={aboutMe.welcome} />
     </div>
   );
 }
