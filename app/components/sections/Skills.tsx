6dc6912dd5cb95920205060162470551d8a9c5ed
nahK994 <nkskl6@gmail.com>
2025-04-08 13:56:46 +0600

    update style in skill section

--- app/components/sections/Skills.tsx
+++ app/components/sections/Skills.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 
 function SkillCard({ skill }: { skill: SkillInfo }) {
-    return <div className="bg-white shadow-lg w-full md:max-w-2/5 rounded-2xl">
+    return <div className="bg-white shadow-lg w-full md:max-w-1/4 rounded-2xl">
         <h2 className="text-xl font-bold mb-4 rounded-t-2xl text-gray-600 bg-amber-100 p-4">{skill.label}</h2>
         <div className="flex flex-wrap gap-4 px-4 pb-4">
             {skill.items.map((item) => (
@@ -20,13 +20,13 @@ export default function Skills({ skill, sectionId }: { skill: SkillCategory; sec
     return (
         <section id={sectionId} className="min-h-screen flex items-center justify-center bg-gradient-to-b from-green-50 to-yellow-50">
             <div className="min-h-screen flex flex-col items-center justify-center py-10">
-                <div className="flex flex-row flex-wrap justify-center gap-6 px-4 max-w-5xl">
+                <div className="flex flex-row flex-wrap justify-center gap-6 px-4 max-w-full">
                     <SkillCard key={skill.language.label} skill={skill.language} />
                     <SkillCard key={skill.backend.label} skill={skill.backend} />
+                    <SkillCard key={skill.frontend.label} skill={skill.frontend} />
                     <SkillCard key={skill.database.label} skill={skill.database} />
                     <SkillCard key={skill.devops.label} skill={skill.devops} />
                     <SkillCard key={skill.api.label} skill={skill.api} />
-                    <SkillCard key={skill.frontend.label} skill={skill.frontend} />
                     <SkillCard key={skill.tools.label} skill={skill.tools} />
                 </div>
             </div>
