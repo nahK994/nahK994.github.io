147bbb80b8e4b3621554952fd3c4db71d421e823
nahK994 <nkskl6@gmail.com>
2025-04-11 22:04:47 +0600

    update commands.sh

--- commands.sh
+++ commands.sh
@@ -2,6 +2,7 @@
 
 # Set Variables
 DEPLOYMENT_NAME="go-app"
+POD_NAME="go-app-pod"
 IMAGE_NAME="go-app-image"
 NEW_IMAGE_TAG="v2"
 PORT=8000
@@ -11,8 +12,9 @@ kubectl create deployment $DEPLOYMENT_NAME --image=$IMAGE_NAME:$NEW_IMAGE_TAG
 
 # Expose the Deployment
 # Use LoadBalancer for cloud or NodePort for local Minikube/kind
-kubectl expose deployment $DEPLOYMENT_NAME --type=LoadBalancer --port=$PORT || \
+kubectl expose deployment $DEPLOYMENT_NAME --type=LoadBalancer --port=$PORT
 kubectl expose deployment $DEPLOYMENT_NAME --type=NodePort --port=$PORT
+minikube service $DEPLOYMENT_NAME
 
 # Scale Deployment
 kubectl scale deployment $DEPLOYMENT_NAME --replicas=3
@@ -21,6 +23,11 @@ kubectl scale deployment $DEPLOYMENT_NAME --replicas=3
 kubectl get deployments
 kubectl get services
 kubectl get pods
+
+# Inspect Deployment, Service, and Pod
+kubectl describe deployment $DEPLOYMENT_NAME
+kubectl describe service $DEPLOYMENT_NAME
+kubectl describe pod $POD_NAME
 
 # Update Deployment Image (Rolling Update)
 kubectl set image deployment/$DEPLOYMENT_NAME $DEPLOYMENT_NAME=$IMAGE_NAME:$NEW_IMAGE_TAG
